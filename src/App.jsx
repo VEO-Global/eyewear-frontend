0f30d76d6c75a4503700c7902a31769a06346f2f
duyquangn84@gmai.com <duyquangn84@gmail.com>
2026-01-08 08:36:55 +0700

    Install dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 function App() {
   const styleCenter = {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center', // Canh giữa theo chiều dọc
-    alignItems: 'center',     // Canh giữa theo chiều ngang
-    height: '100vh',          // Chiều cao bằng toàn bộ màn hình
-    width: '100vw',           // Chiều rộng bằng toàn bộ màn hình
-    position: 'fixed',        // Cố định vị trí để bỏ qua margin mặc định của body
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center", // Canh giữa theo chiều dọc
+    alignItems: "center", // Canh giữa theo chiều ngang
+    height: "100vh", // Chiều cao bằng toàn bộ màn hình
+    width: "100vw", // Chiều rộng bằng toàn bộ màn hình
+    position: "fixed", // Cố định vị trí để bỏ qua margin mặc định của body
     top: 0,
     left: 0,
     margin: 0,
-    textAlign: 'center'
+    textAlign: "center",
   };
 
   return (
     <div style={styleCenter}>
-      <h1>🚀 Hello World!</h1>
-      <h2>Welcome to VEO Eyewear Project</h2>
+      <h1 className="text-6xl font-serif mb-6">🚀 Hello World!</h1>
+      <h2 className="text-3xl font-serif tracking-widest">
+        Welcome to VEO Eyewear Project
+      </h2>
       <p>System is running...</p>
     </div>
   );
