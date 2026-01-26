70b54843ab9baa11d3c3013a65927dde8c319bf1
duyquangn84@gmai.com <duyquangn84@gmail.com>
2026-01-26 08:07:37 +0700

    Adding configs

---
+++ src/configs/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "system-ui", "sans-serif"],
+        serif: ["Noto Serif", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
