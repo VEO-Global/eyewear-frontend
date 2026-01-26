e6dffe2ebbd6ad42cfcc908904ae09561a05fc4a
duyquangn84@gmai.com <duyquangn84@gmail.com>
2026-01-26 07:41:53 +0700

    Add AppRouter Component for App.jsx , add FontStyle Merriweather in index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
-function App() {
-  const styleCenter = {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center", // Canh giữa theo chiều dọc
-    alignItems: "center", // Canh giữa theo chiều ngang
-    height: "100vh", // Chiều cao bằng toàn bộ màn hình
-    width: "100vw", // Chiều rộng bằng toàn bộ màn hình
-    position: "fixed", // Cố định vị trí để bỏ qua margin mặc định của body
-    top: 0,
-    left: 0,
-    margin: 0,
-    textAlign: "center",
-  };
+import HomePages from "./pages/HomePages";
+import AppRouter from "./router/AppRouter";
 
+function App() {
   return (
-    <div style={styleCenter}>
-      <h1 className="text-6xl font-serif mb-6">🚀 Hello World!</h1>
-      <h2 className="text-3xl font-serif tracking-widest">
-        Welcome to VEO Eyewear Project
-      </h2>
-      <p>System is running...</p>
+    <div className="min-h-screen bg-white font-sans text-gray-900">
+      <AppRouter />
     </div>
   );
 }
