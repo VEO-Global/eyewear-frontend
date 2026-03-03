0e03ddce1107503c8cd8c73ba3d03e9006a6dad1
duyquangn84@gmai.com <duyquangn84@gmail.com>
2026-03-03 19:00:46 +0700

    Adding components for productions page

---
+++ src/components/storepage/Title.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Title() {
+  return (
+    <div className="px-4 py-8 text-center md:text-left">
+      <h2 className="text-3xl font-extrabold text-gray-900 mb-2">
+        Bộ sưu tập của chúng tôi😎{" "}
+      </h2>
+      <p className="text-gray-600">
+        Khám phá những chiếc gọng kính hoàn hảo cho phong cách độc đáo của bạn.{" "}
+      </p>
+    </div>
+  );
+}
