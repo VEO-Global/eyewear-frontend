abab17b143e1d9745d015f279297f086cf46a23e
duyquangn84@gmai.com <duyquangn84@gmail.com>
2026-03-11 21:09:55 +0700

    adding folder products in redux

---
+++ src/redux/products/producSlice.js
@@ -0,0 +1,72 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "../../configs/config-axios";
+
+const initialState = {
+  products: [],
+  selectedProduct: null,
+  loading: false,
+  error: null,
+};
+
+export const fetchProducts = createAsyncThunk("products/fetch", async (_, { rejectWithValue }) => {
+  try {
+    const response = await api.get("/products");
+    return response.data;
+  } catch (error) {
+    return rejectWithValue(error.response?.data?.message || "Không thể lấy danh sách sản phẩm");
+  }
+});
+
+export const fetchProductById = createAsyncThunk("products/fetchById", async (id, { rejectWithValue }) => {
+  try {
+    const response = await api.get(`/products/${Number(id)}`);
+    const variants = await api.get(`/variants/product/${Number(id)}`);
+
+    const productDetail = {
+      ...response.data,
+      variants: variants.data,
+    };
+
+    return productDetail;
+  } catch (error) {
+    return rejectWithValue(error.response?.data?.message || "Không thể lấy sản phẩm");
+  }
+});
+
+const productSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.products = action.payload;
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+
+      .addCase(fetchProductById.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+
+      .addCase(fetchProductById.fulfilled, (state, action) => {
+        state.loading = false;
+        state.selectedProduct = action.payload;
+      })
+
+      .addCase(fetchProductById.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default productSlice.reducer;
