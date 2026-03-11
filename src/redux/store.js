de5bd4fee3ffa1dbc13b6d4191f4660b45a5f9b8
duyquangn84@gmai.com <duyquangn84@gmail.com>
2026-03-11 21:00:25 +0700

    Adding 2 more Recudec products and cart

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "./auth/authSlice";
+import productReducer from "./products/producSlice";
+import cartReducer from "./cart/cartSlice";
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    products: productReducer,
+    cart: cartReducer,
   },
 });
 
