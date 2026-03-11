b3cd149a27bb75d0744cc9184cdac908f105210d
duyquangn84@gmai.com <duyquangn84@gmail.com>
2026-03-11 20:44:51 +0700

    Fetch User profile when localStore Exsit Token

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import { useDispatch } from "react-redux";
 import AppRouter from "./router/AppRouter";
+import { useEffect } from "react";
+import { fetchProducts } from "./redux/products/producSlice";
+import { fetchProfile } from "./redux/auth/authSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    dispatch(fetchProfile());
+  }
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
   return (
     <div className="min-h-screen bg-white font-sans text-gray-900">
       <AppRouter />
