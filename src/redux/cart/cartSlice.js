4cd413a3fcc35a1c41a0f1491739bd5e62a5ae7a
duyquangn84@gmai.com <duyquangn84@gmail.com>
2026-03-11 21:05:47 +0700

    create cart and product folders in redux

---
+++ src/redux/cart/cartSlice.js
@@ -0,0 +1,71 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [],
+  totalProduct: 0,
+  totalPrice: 0,
+};
+
+function calculateCart(state) {
+  state.totalProduct = state.cart.reduce((total, item) => total + item.quantity, 0);
+
+  state.totalPrice = state.cart.reduce((total, item) => total + item.variantPrice * item.quantity, 0);
+}
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      const product = action.payload;
+      const exist = state.cart.find((item) => item.variantID === product.variantID);
+
+      if (exist) {
+        exist.quantity += 1;
+      } else {
+        state.cart.push({ ...product, quantity: 1 });
+      }
+
+      state.totalProduct += 1;
+      state.totalPrice += product.price;
+
+      calculateCart(state);
+    },
+
+    removeItem(state, action) {
+      const variantId = action.payload;
+
+      const item = state.cart.find((i) => i.variantId === variantId);
+
+      if (item) {
+        state.totalProduct -= item.quantity;
+        state.totalPrice -= item.price * item.quantity;
+      }
+
+      state.cart = state.cart.filter((item) => item.variantId !== variantId);
+    },
+
+    updateQuantity(state, action) {
+      const { variantId, type } = action.payload;
+
+      const item = state.cart.find((i) => i.variantID === variantId);
+
+      if (!item) return;
+
+      if (type === "+") item.quantity += 1;
+      if (type === "-" && item.quantity > 1) item.quantity -= 1;
+
+      calculateCart(state);
+    },
+
+    clearCart(state) {
+      state.cart = [];
+      state.totalProduct = 0;
+      state.totalPrice = 0;
+    },
+  },
+});
+
+export const { addItem, removeItem, updateQuantity, clearCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
