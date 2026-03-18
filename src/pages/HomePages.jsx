e682d63cdcb9b1157e46bf04e9eded6a28fd1587
duyquangn84@gmai.com <duyquangn84@gmail.com>
2026-03-18 21:25:42 +0700

    Move fetch function form App.jsx to HomePage.jsx

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ServiceCards } from "../components/homepage/ServiceCard";
 import { Header } from "../components/common/Header";
 import { HeroSection } from "../components/homepage/HeroSection";
 import { CustomGlassesProcess } from "../components/homepage/CustomGlassesProcess";
 import { WhyChooseUs } from "../components/homepage/WhyChooseUs";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllCategories } from "../redux/category/categorySlice";
+import { fetchProducts } from "../redux/products/producSlice";
 // import ProductionGrid from "../components/homepage/ProductionGrid";
 
 export default function HomePages() {
+  const dispatch = useDispatch();
+  const { products } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+    dispatch(fetchAllCategories());
+  }, [dispatch]);
+
   return (
     <div>
       <HeroSection />
