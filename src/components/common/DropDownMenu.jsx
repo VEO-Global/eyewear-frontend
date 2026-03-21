0db86330795b746723887a700363d1fa3466e931
duyquangn84@gmai.com <duyquangn84@gmail.com>
2026-03-21 15:11:22 +0700

    Fixing when user want to navigate to profile paeg

--- src/components/common/DropDownMenu.jsx
+++ src/components/common/DropDownMenu.jsx
@@ -1,16 +1,18 @@
 import { LogOut, UserCircle } from "lucide-react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 export default function DropDownMenu({ openMenu, setOpenMenu, handleLogout }) {
-  if (!openMenu) return null;
+  const { user } = useSelector((state) => state.auth);
 
+  if (!openMenu) return null;
   return (
     <div
       className="abs right-0 mt-2 w-52 bg-white border rounded-xl shadow-lg py-2 z-50"
       onMouseLeave={() => setOpenMenu(false)}
     >
       <Link
-        to="/user/profile"
+        to={`/user/profile/${user.id}`}
         className="flex items-center gap-2 px-4 py-2 text-sm hover:bg-gray-100"
       >
         <UserCircle className="w-4 h-4" />
