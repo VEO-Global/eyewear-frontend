96c25fe964b6ca20d3dc77fd38fa73b3bb12ed70
duyquangn84@gmai.com <duyquangn84@gmail.com>
2026-03-21 14:48:50 +0700

    Update code for HomePage.jsx

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -4,17 +4,14 @@ import { Header } from "../components/common/Header";
 import { HeroSection } from "../components/homepage/HeroSection";
 import { CustomGlassesProcess } from "../components/homepage/CustomGlassesProcess";
 import { WhyChooseUs } from "../components/homepage/WhyChooseUs";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { fetchAllCategories } from "../redux/category/categorySlice";
-import { fetchProducts } from "../redux/products/producSlice";
 // import ProductionGrid from "../components/homepage/ProductionGrid";
 
 export default function HomePages() {
   const dispatch = useDispatch();
-  const { products } = useSelector((state) => state.products);
 
   useEffect(() => {
-    dispatch(fetchProducts());
     dispatch(fetchAllCategories());
   }, [dispatch]);
 
